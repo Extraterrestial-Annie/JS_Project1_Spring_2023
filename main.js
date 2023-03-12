e607a6af6886f123129b091470ea43ebb6a308df
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-03-12 11:02:43 +0200

    Got started

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const taskList = [];
+
+function addObject() {
+    var x = document.getElementById("myText");
+    if (x.length < 3) {
+        alert("Task is too short");
+    } else {
+        push.taskList(x)
+        localStorage.
+    }
+}
+function printTasks() {
+    taskList.forEach(printTask())
+}
+function printTask() {
+
+}
